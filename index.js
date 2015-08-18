ac8190edb8ec4b26e0359507758f151af1a523b3
Harut Martirosyan <harut.martirosyan@gmail.com>
2015-08-18 11:02:34 +0300

    Update index.js
    Removed sample code

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
 var winston = require('winston');
 
 require('./lib/winston-graphite-tcp.js');
-
-winston.add(winston.transports.Graphite, {
-    port: 2003,
-    host: 'graph1'
-});
-winston.remove(winston.transports.Console);
-
-setTimeout(function() {
-    winston.info("k.b.c.d.e 1");
-}, 5000)
-
